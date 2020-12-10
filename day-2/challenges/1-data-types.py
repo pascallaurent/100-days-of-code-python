8769bc13db9e7886c6e193a67e8132a474aaf72e
Pascal Laurent <tipizo@gmail.com>
2020-12-10 01:41:24 +0000

    day 2: data types

---
+++ day-2/challenges/1-data-types.py
@@ -0,0 +1,7 @@
+two_digit_number = input("Type a two digit number: ")
+
+num1 = int(two_digit_number[0])
+num2 = int(two_digit_number[1])
+result = num1 + num2
+
+print(str(num1) + " + " + str(num2) + " = " + str(result))
